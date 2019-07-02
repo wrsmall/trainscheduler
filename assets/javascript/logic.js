db63afe96f61700cb391a42e65fd0f316dc6abfc
william Small <william.r.small89@gmail.com>
2019-07-02 17:01:23 -0400

    need to get button working

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -6,7 +6,7 @@ var config = {
     storageBucket: "trainschedu-870f9.firebaseapp.com",
     messagingSenderId: "993779793345",
     appId: "1:993779793345:web:30e2b033bfd895e1"
-  };
+};
 
 firebase.initializeApp(config);
 
@@ -17,7 +17,7 @@ $("#addTrain").on("click", function (event) {
 
     var train = $("#trainName").val().trim();
     var destination = $("#destination").val().trim();
-    var firstTrain = $("#firstTrain").val().trim();
+    var firstTrain = moment($("#firstTrain").val().trim(), "HH:mm").format("X");
     var frequency = $("#trainFrequency").val().trim();
 
     var newTrain = {
@@ -28,53 +28,67 @@ $("#addTrain").on("click", function (event) {
     };
     database.ref().push(newTrain);
 
-  // Logs everything to console
-  console.log(newTrain.trainId);
-  console.log(newTrain.destined);
-  console.log(newTrain.first);
-  console.log(newTrain.freq);
+    // Logs everything to console
+    console.log(newTrain.trainId);
+    console.log(newTrain.destined);
+    console.log(newTrain.first);
+    console.log(newTrain.freq);
 
 
-  // Clears all of the text-boxes
-  $("#trainName").val("");
-  $("#destination").val("");
-  $("#firstTrain").val("");
-  $("#trainFrequency").val("");
+    // Clears all of the text-boxes
+    $("#trainName").val("");
+    $("#destination").val("");
+    $("#firstTrain").val("");
+    $("#trainFrequency").val("");
 });
-database.ref().on("child_added", function(childSnapshot) {
+database.ref().on("child_added", function (childSnapshot) {
     console.log(childSnapshot.val());
-  
+
     // Store everything into a variable.
     var addTrain = childSnapshot.val().trainId;
     var addDestination = childSnapshot.val().destined;
     var addFirstTrain = childSnapshot.val().first;
     var addFrequency = childSnapshot.val().freq;
-   
 
-  
+    var trainFreq =addFrequency;
+    var startTime = addFirstTrain;
+    var convert = moment(startTime, "hh:mm").subtract(1, "years");
+    var timeDiff = moment().diff(moment(convert), "minutes");
+    var remainInter = timeDiff % trainFreq;
+    var minutesRemain = trainFreq - remainInter;
+    
+    console.log(minutesRemain)
+    
+    var nextTrain = moment().add(minutesRemain, "minutes").format("hh:mm");
+
+    console.log(nextTrain)
+
     // Employee Info
     console.log(addTrain);
     console.log(addDestination);
     console.log(addFirstTrain);
     console.log(addFrequency);
-  
-  
+
+
     // Create the new row
     var newRow = $("<tr>").append(
-      $("<td>").text(addTrain),
-      $("<td>").text(addDestination),
-      $("<td>").text(addFirstTrain),
-      $("<td>").text(addFrequency),
-      $("<td>").html("<button id='removeTrain' class='rounded bg-danger'>"+"Remove Train"+"</button>"),
-      
+        $("<td>").text(addTrain),
+        $("<td>").text(addDestination),
+        $("<td>").text(addFrequency),
+        $("<td>").text(nextTrain),
+        $("<td>").text(minutesRemain),
+        $("<td>").html("<button id='removeTrain' class='rounded bg-danger'>" + "Remove Train" + "</button>"),
+
     );
-  
+
 
     // Append the new row to the table
     $("#trainTable > tbody").append(newRow);
-  });
 
-  $("#removeTrain").on("click", function(){
-    ref.child("this").removeValue();
+    $("#removeTrain").on("click", function () {
+        ref.child($(this)).remove();
 
-  });
+    });
+
+});
+
